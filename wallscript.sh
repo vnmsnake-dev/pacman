04fed8f15d1357e1d8ae465382d6ad8258dbae5f
vnmsnake-dev <129844403+vnmsnake-dev@users.noreply.github.com>
2023-04-24 05:40:18 +0000

    Add files via upload

---
+++ wallscript.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+wget https://e1.pxfuel.com/desktop-wallpaper/54/60/desktop-wallpaper-itachi-uchiha-moonlit-night-live-itachi-on-pole.jpg
+mv desktop-wallpaper-itachi-uchiha-moonlit-night-live-itachi-on-pole.jpg /home/vnmsnake/pacman/itachi.jpg
+mv  /home/vnmsnake/pacman/config.py /home/vnmsnake/.config/qtile
